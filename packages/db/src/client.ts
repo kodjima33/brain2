eef774ada584b0f2ffc247d1cce681176d73f9db
Gram Liu <gram.liu226@gmail.com>
2023-12-21 16:23:53 +0800

    Initialize db schema

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,21 @@
+import type { AudioBlob } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
+
 export * from "@prisma/client";
 
 export const prisma = new PrismaClient({
   log: ["error"],
+}).$extends({
+  // Add computed field for the S3 path
+  // TODO: update file extension as needed
+  result: {
+    audioBlob: {
+      s3Path: {
+        needs: { id: true },
+        compute(audioBlob: AudioBlob) {
+          return `audio/${audioBlob.id}.wav`;
+        },
+      },
+    },
+  },
 });
