1afa958241d2952c809032c8dfce6db24a719620
Gram Liu <gram.liu226@gmail.com>
2023-12-23 18:15:13 +0800

    Update default icon

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -25,7 +25,7 @@ const defineConfig = (): ExpoConfig => ({
     package: "your.bundle.identifier",
     adaptiveIcon: {
       foregroundImage: "./assets/icon.png",
-      backgroundColor: "#1F104A",
+      backgroundColor: "#FFFFFF",
     },
   },
   // extra: {
