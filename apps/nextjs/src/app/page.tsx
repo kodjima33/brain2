93e3a656a43e65d407a2f632a19b32f3427b5361
Gram Liu <gram.liu226@gmail.com>
2024-01-08 23:07:46 -0800

    Use long-lived jwt template

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -4,7 +4,9 @@ import ClipboardButton from "~/components/clipboard-button";
 
 export default async function HomePage() {
   const { getToken } = auth();
-  const token = await getToken();
+  const token = await getToken({
+    template: "dev_long_lived",
+  });
   const user = await currentUser();
 
   return (
@@ -14,7 +16,7 @@ export default async function HomePage() {
           <h1 className="text-3xl font-bold tracking-tight sm:text-[5rem]">
             Welcome, {user?.firstName} {user?.lastName}
           </h1>
-          <div className="mt-10 flex flex-row gap-5 items-center justify-center">
+          <div className="mt-10 flex flex-row items-center justify-center gap-5">
             <span className="text-lg">Click to copy your Clerk JWT:</span>
             <ClipboardButton text={token!} />
           </div>
