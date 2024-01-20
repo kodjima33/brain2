57d1821d2e1ebf0738073febc6bd4be54a63a044
Anuda Weerasinghe <anuda@live.com>
2024-01-20 14:07:21 -0500

    implements frontend messenger account linking call

--- apps/nextjs/src/app/messenger-auth/page.tsx
+++ apps/nextjs/src/app/messenger-auth/page.tsx
@@ -1,25 +1,61 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { useUser } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
+import axios from "axios";
+
+import { env } from "~/env";
+
+interface LinkRequest {
+  messengerPSID: string;
+}
 
 export default function MessengerAuth() {
   const searchParams = useSearchParams();
 
-  const accountLinkingToken = searchParams.get("account_linking_token");
-  const redirectURI = searchParams.get("redirect_uri");
+  const messengerPSID = searchParams.get("messengerPSID");
 
-  const { isLoaded, isSignedIn, user } = useUser();
+  const { isSignedIn, getToken } = useAuth();
+
+  const [authComplete, setAuthComplete] = useState(false);
+
+  async function linkMessengerAccount() {
+    if (!messengerPSID) {
+      return;
+    }
+
+    const token = await getToken();
+
+    const request: LinkRequest = { messengerPSID: messengerPSID };
+
+    await axios.post(
+      `${env.VERCEL_URL}/api/conversations/messenger-auth`,
+      request,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    setAuthComplete(true);
+  }
 
   useEffect(() => {
-    console.log(isLoaded, isSignedIn, user);
-  }, [isLoaded, isSignedIn, user]);
+    if (isSignedIn) {
+      void linkMessengerAccount();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSignedIn]);
   return (
     <div>
-      <h1>
-        {accountLinkingToken}, {redirectURI}, {user?.id}
-      </h1>
+      <h3>
+        {authComplete
+          ? "Your messenger account has been successfully linked! You're ready to start chatting with Brain² on Messenger :)"
+          : "Loading..."}
+      </h3>
     </div>
   );
 }
