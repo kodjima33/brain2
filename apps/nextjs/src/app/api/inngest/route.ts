64cb2f94767870b1efe475cfd4276a14a02b0501
Gram Liu <gram.liu226@gmail.com>
2024-01-20 18:16:12 -0800

    Remove cleaved api endpoints

--- apps/nextjs/src/app/api/inngest/route.ts
+++ apps/nextjs/src/app/api/inngest/route.ts
@@ -1,7 +1,6 @@
 import { inngestEdgeClient } from "@brain2/lib/queue/clients";
 import { handlers } from "@brain2/lib/queue/functions";
 import { serve } from "inngest/next";
-import { env } from "~/env";
 
 export const runtime = "edge";
 
@@ -9,5 +8,4 @@ export const { GET, POST, PUT } = serve({
   client: inngestEdgeClient,
   functions: [handlers.refineNoteTranscriptHandler, handlers.dummyHandler],
   streaming: "allow",
-  serveHost: (env.VERCEL_URL ?? "http://localhost:3000") + "/api/inngest/edge",
 });
