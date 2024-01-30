a58918a050f3c653d52a1e48b295fe05213200c4
Gram Liu <gram.liu226@gmail.com>
2024-01-29 22:48:22 -0800

    Add accelerate to base prisma client

--- packages/db/src/edge.ts
+++ packages/db/src/edge.ts
@@ -10,18 +10,18 @@ export const AUDIO_FORMAT = "m4a";
 export const prisma = new PrismaClient({
   log: ["error"],
 })
-.$extends(withAccelerate())
-.$extends({
-  // Add computed field for the S3 path
-  // TODO: update file extension as needed
-  result: {
-    audioBlob: {
-      s3Path: {
-        needs: { id: true },
-        compute(audioBlob: AudioBlob) {
-          return `audio/${audioBlob.id}.${AUDIO_FORMAT}`;
+  .$extends(withAccelerate())
+  .$extends({
+    // Add computed field for the S3 path
+    // TODO: update file extension as needed
+    result: {
+      audioBlob: {
+        s3Path: {
+          needs: { id: true },
+          compute(audioBlob: AudioBlob) {
+            return `audio/${audioBlob.id}.${AUDIO_FORMAT}`;
+          },
         },
       },
     },
-  },
-});
+  });
