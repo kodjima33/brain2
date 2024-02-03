7dad5e7df0d6a02cc4f15e22d81b952e9ea98c3f
Gram Liu <gram.liu226@gmail.com>
2024-02-03 14:10:21 -0800

    Create basic home page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,15 @@
+import Brain2Icon from "~/icons/Brain2Icon";
+
 export default async function HomePage() {
   return (
-    <main className="flex h-screen flex-col items-center text-black"></main>
+    <main className="flex h-screen w-screen flex-col items-center justify-center text-black">
+      <div className="mb-32 flex flex-col items-center">
+        <Brain2Icon className="h-32" />
+        <div className="mt-10 flex flex-col items-center">
+          <p className="text-3xl font-bold">Brain²</p>
+          <p className="text-xl">Your personalized AI journal</p>
+        </div>
+      </div>
+    </main>
   );
 }
