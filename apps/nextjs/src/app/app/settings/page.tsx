dabf23dc9abee629044f76698fb5c87b5a86fee1
Gram Liu <gram.liu226@gmail.com>
2024-02-03 15:11:08 -0800

    Add sign out logic

--- apps/nextjs/src/app/app/settings/page.tsx
+++ apps/nextjs/src/app/app/settings/page.tsx
@@ -1,11 +1,38 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 "use client";
 
-import { useUser } from "@clerk/nextjs";
+import { useClerk, useUser } from "@clerk/nextjs";
+import { LogOutIcon } from "lucide-react";
 
 import Header from "~/components/header";
+import { cn } from "~/lib/utils";
+
+interface SettingsEntryProps {
+  icon: React.ReactNode;
+  text: string;
+  onClick?: () => void;
+  className?: string;
+}
+
+function SettingsEntry({ icon, text, onClick, className }: SettingsEntryProps) {
+  return (
+    <div
+      className={cn(
+        className,
+        "flex cursor-pointer flex-row items-center gap-5 p-2",
+      )}
+      onClick={onClick}
+    >
+      {icon}
+      <p className="font-semibold">{text}</p>
+    </div>
+  );
+}
 
 export default function SettingsPage() {
   const { user } = useUser();
+  const { signOut } = useClerk();
   if (!user) {
     return null;
   }
@@ -13,6 +40,15 @@ export default function SettingsPage() {
   return (
     <>
       <Header user={user} title="Settings" />
+      <div className="flex w-8/12 flex-col items-start gap-2">
+        <p className="font-bold">Account</p>
+        <SettingsEntry
+          icon={<LogOutIcon />}
+          text="Sign out"
+          className="text-red-500"
+          onClick={signOut}
+        />
+      </div>
     </>
   );
 }
